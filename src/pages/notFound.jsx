19dede627fe9eb157fe2dc38e3280eb78da1a130
Azazek6 <naturaespinola12@gmail.com>
2024-05-08 09:40:45 -0500

    fixes

---
+++ src/pages/notFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from "../layouts/Layout";
+
+const notFound = () => {
+  return (
+    <Layout>404</Layout>
+  )
+}
+
+export default notFound
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import notFound from "../pages/notFound";
 import Home from "../pages/home";
 import About from "../pages/about";
 
@@ -8,6 +9,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<notFound />} />
       </Routes>
     </BrowserRouter>
   );
